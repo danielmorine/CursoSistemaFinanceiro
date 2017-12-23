a1837f41d0f3d34f299dba502df8935aa2b15184
Daniel Haro <contato.danielharo@gmail.com>
2017-12-23 16:03:24 -0200

    Iniciando Projeto em Asp.Net Mvc, padrao 3 Camadas no Curso de Asp.Net Mvc com Hugo Vasconcelos

---
+++ Model.Dao/ConexaoDB.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace Model.Dao
+{
+    class ConexaoDB
+    {
+
+        //CRIANDO A CONEXAO COM O DB, INICIA NULL
+        private static ConexaoDB objConexaoDB = null;
+        private SqlConnection con;
+
+        //CRIAR CONSTRUTOR E CONFIGURAR STRING DE CONEXAO COM O BANCO
+        private ConexaoDB()
+        {
+            con = new SqlConnection("Data Source=DESKTOP-RNV9ESI; Initial Catalog=Financeiro; Integrated Security=True");
+        }
+        //SABER O ESTADO DA CONEXAO (INICIAR A CONEXAO E ENCERRAR A CONEXAO)   
+        public static ConexaoDB saberEstado()
+        {
+            if(objConexaoDB == null)
+            {
+
+            }   
+        }
+    }
+}
